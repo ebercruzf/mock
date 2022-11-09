68c7607310fe1261866e5a53d4a57ed6610db236
ebercruz <ebercruzdev@gmail.com>
2022-11-08 21:45:28 -0600

    origen de los tiempos

---
+++ src/main/java/com/app/mock/request/PersonRequestDTO.java
@@ -0,0 +1,10 @@
+package com.app.mock.request;
+
+import lombok.Data;
+
+@Data
+public class PersonRequestDTO {
+
+    private String name;
+    private String message;
+}
---
+++ src/main/java/com/app/mock/response/PersonResponseDTO.java
@@ -0,0 +1,11 @@
+package com.app.mock.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PersonResponseDTO {
+    private String responseName;
+    private String responseMessage;
+}
